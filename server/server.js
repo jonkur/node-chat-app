2dfc96877110cd9d6b383990b53b83054d71e3f7
jonkur <jonkur@users.noreply.github.com>
2017-08-01 14:01:56 +0300

    Add join page that submits to chat.html

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public/');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
